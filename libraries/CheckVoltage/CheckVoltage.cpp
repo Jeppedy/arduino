6b47f85d9f1d398408970b5bab5c81447f3ec5bd
Jeppedy <jeff@theherrs.com>
2016-03-02 23:41:25 -0600

    Removed Unneeded code

--- libraries/CheckVoltage/CheckVoltage.cpp
+++ libraries/CheckVoltage/CheckVoltage.cpp
@@ -16,28 +16,3 @@ int checkVoltage( long int _intRefVoltage ) {
 
    return results;
 }
-
-// results are Vcc * 100: So for example, 5V would be 500.
-int ORIG_checkVoltage( int origADCSRA, long int _intRefVoltage ) {
-//   int curr_adcsra = ADCSRA ;
-//  printf("Entrance ADCSRA = %d\n", ADCSRA ) ;
-//  printf("passed ADCSRA = %d\n", origADCSRA) ;
-//   power_adc_enable();
-//   ADCSRA=origADCSRA ;
-
-   // REFS0 : Selects AVcc external reference
-   // MUX3 MUX2 MUX1 : Selects 1.1V (VBG)
-   ADMUX = bit (REFS0) | bit (MUX3) | bit (MUX2) | bit (MUX1);
-	   delayMicroseconds( 250 ) ;  // Some reference suggested this helps
-   ADCSRA |= bit( ADSC );  // start conversion
-   while (ADCSRA & bit (ADSC))
-     { }  // wait for conversion to complete
-   word x = ADC ;
-   int results = (((_intRefVoltage * 1024) / x) + 5) / 10;
-
-//   ADCSRA=curr_adcsra;
-//  printf("Exit ADCSRA = %d\n", ADCSRA ) ;
-
-   return results;
-}
-
